a33acc407ae6b8cb742ba1b2e7052fd832e84f73
komihal <kozlovma77@gmail.com>
2023-01-27 23:54:53 +0300

    hi

--- main.py
+++ main.py
@@ -20,12 +20,16 @@ import streamlit as st
 # sheet = "TDSheet"
 if "spreadsheet_id" not in st.session_state:
     st.session_state["spreadsheet_id"] = '1O0qVHr2vkTjpGDyOao1gT1ULPDDYbYCjj0mC8fhnVVA'
+if "link" not in st.session_state:
+    st.session_state["link"] = ""
 
 CREDENTIALS_FILE = "cred_service.json"
 
 tab1, tab2, tab3 = st.tabs(["Загрузка морс", "Загрузка статусов", "Загрузка еще чего-нибудь"])
-
-st.sidebar.write("Текущая ссылка на файл spreadsheet: " + "https://docs.google.com/spreadsheets/d/" + st.session_state["spreadsheet_id"])
+if st.session_state["link"] != "":
+    st.sidebar.write("Текущая ссылка на файл spreadsheet: " + "https://docs.google.com/spreadsheets/d/" + st.session_state["link"])
+else:
+    st.sidebar.write("Текущая ссылка на файл spreadsheet: " + "https://docs.google.com/spreadsheets/d/" + st.session_state["spreadsheet_id"])
 
 with tab1:
 
@@ -35,7 +39,7 @@ with tab1:
     with col2:
         st.text_input(
             "Путь к гугл файлу: ", value="",
-            max_chars=None, key=None, type="default", help="при добавлении ID нового файла, заблаговременно откройте к нему доступ для служебного аккаунта: account@databasealpha.iam.gserviceaccount.com", autocomplete=None, on_change=None,
+            max_chars=None, key="link", type="default", autocomplete=None, on_change=None,
             args=None, kwargs=None, placeholder="при необходимости введите новый  гугл ID spreadsheet", disabled=False,
             label_visibility="collapsed")
     with col3:
@@ -43,12 +47,10 @@ with tab1:
     st.write("* при добавлении ID нового файла, заблаговременно откройте к нему доступ для служебного аккаунта: "
              "account@databasealpha.iam.gserviceaccount.com")
     if st.button:
-        st.session_state["spreadsheet_id"] = st.text_input
-
+        st.session_state["spreadsheet_id"] = st.session_state["link"]
     if uploaded_mors:
 
         df = pd.read_excel(uploaded_mors)
-
         df.columns = df.iloc[7] # название столбцов
         df = df.dropna(axis=1, how='all')  # дроп пустых
         df = df.drop(df.index[0:8])  # дроп первых 8
@@ -63,24 +65,24 @@ with tab1:
         df = df.drop(["index"], axis=1) # нумерация индексов
         df = df.fillna("")
 
+        try:
+            credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE,
+                ['https://www.googleapis.com/auth/spreadsheets',
+                 'https://www.googleapis.com/auth/drive'])
+            httpAuth = credentials.authorize(httplib2.Http())
+            service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
 
-
-
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE,
-            ['https://www.googleapis.com/auth/spreadsheets',
-             'https://www.googleapis.com/auth/drive'])
-        httpAuth = credentials.authorize(httplib2.Http())
-        service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
-
-        response_date = service.spreadsheets().values().update(
-            spreadsheetId=st.session_state["spreadsheet_id"],
-            range='A1:EE',
-            valueInputOption='RAW',
-            body=dict(
-                majorDimension='ROWS',
-                values=df.values.tolist()[:])
-        ).execute()
-        st.write("файл обновлен по ссылке: " + "https://docs.google.com/spreadsheets/d/" + str(st.session_state["spreadsheet_id"]))
+            response_date = service.spreadsheets().values().update(
+                spreadsheetId=st.session_state["spreadsheet_id"],
+                range='A1:EE',
+                valueInputOption='RAW',
+                body=dict(
+                    majorDimension='ROWS',
+                    values=df.values.tolist()[:])
+            ).execute()
+            st.write("файл обновлен по ссылке: " + "https://docs.google.com/spreadsheets/d/" + st.session_state["spreadsheet_id"])
+        except:
+            st.write("Неудача!!! попробуй поменять ID таблицы или открыть доступ к ней служебному аккаунту account@databasealpha.iam.gserviceaccount.com")
     else:
         st.write("")
 
